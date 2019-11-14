34b034c62b7bd635a8f18450e37b478a78d4f695
wumingwei <wu_mingwei@yeah.net>
2019-11-14 16:22:55 +0800

    init project.

---
+++ utils/util.js
@@ -0,0 +1,21 @@
+// 格式化日期时间
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+// 补零转化
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+module.exports = {
+  formatTime: formatTime
+}
